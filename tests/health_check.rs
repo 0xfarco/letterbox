91b1439a8d142ebbeb9f2d2ec888cb391273730f
0xfarco <0xfarco@gmail.com>
2025-10-16 09:08:29 +0530

    rearrange /health_check.rs & correct a typo

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,20 +2,6 @@
 
 use std::net::TcpListener;
 
-#[tokio::test]
-async fn health_check_works() {
-    let address = spawn_app();
-    let client = reqwest::Client::new();
-    let response = client
-        .get(&format!("{}/health_check", &address))
-        .send()
-        .await
-        .expect("Failed to execut request.");
-    
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
-}
-
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
@@ -23,3 +9,22 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
