c1a23adc06a83c9a2f1a781806d9c0c8f722be4a
0xfarco <0xfarco@gmail.com>
2025-10-20 11:01:10 +0530

    enforce formatting

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,12 +1,18 @@
-use tracing_subscriber::fmt::MakeWriter;
 use tracing::Subscriber;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
+use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 
-pub fn get_subscriber<Sink>(name: String, env_filter: String, sink: Sink) -> impl Subscriber + Sync + Send 
-    where Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static, {
+pub fn get_subscriber<Sink>(
+    name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Sync + Send
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new(name, sink);
 
